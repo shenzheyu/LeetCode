d6544ef341b0705e7de587c59941ed05167b6399
shenzheyu <shenzheyu96@gmail.com>
2018-05-05 22:23:24 +0800

    complete 46

---
+++ 046-permutations/src/SolutionTest.java
@@ -0,0 +1,64 @@
+import org.junit.Test;
+
+import java.util.List;
+
+public class SolutionTest {
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    @Test
+    public void permuteTest() {
+
+        int[] nums = stringToIntegerArray("[1,2,3]");
+
+        List<List<Integer>> ret = new Solution().permute(nums);
+
+        String out = int2dListToString(ret);
+
+        System.out.print(out);
+
+    }
+
+}
